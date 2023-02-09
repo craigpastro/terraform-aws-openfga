07aed08b8301281dc8777f69f577abbb4dfc8ede
Craig Pastro <craig.pastro@okta.com>
2023-02-08 18:59:11 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "random_pet" "this" {}
 locals {
   name = "openfga-${random_pet.this.id}"
 
-  db_conn_string = "postgres://${var.db_username}:${var.db_password}@${aws_rds_cluster_instance.this[0].endpoint}/${var.db_name}"
+  db_conn_string = var.db_type == "postgres" ? "postgres://${var.db_username}:${var.db_password}@${aws_rds_cluster_instance.this[0].endpoint}/${var.db_name}" : ""
 
   tags = merge({
     Name      = local.name,
